a57838681874e285e4162907d4e259d59b5d8b45
Jeferson Narvaez <jeferson.narvaez@kuskipagos.com>
2021-12-29 21:30:45 -0500

    Parse Complex Json to Kotlin Object and show in screen

--- app/src/main/java/com/example/recyclerview/MainViewModel.kt
+++ app/src/main/java/com/example/recyclerview/MainViewModel.kt
@@ -6,19 +6,46 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
+import org.json.JSONObject
 
-class MainViewModel:ViewModel() {
+class MainViewModel : ViewModel() {
     private val _eqList = MutableLiveData<MutableList<Earthquake>>()
     val eqList: LiveData<MutableList<Earthquake>> = _eqList
 
     init {
         viewModelScope.launch {
-            val eqList= service.getLastHourEarthquake()
-            Log.i("REQUEST",eqList)
+            val response = service.getLastHourEarthquake()
+            val eqList = parseStringToJson(response)
+            _eqList.postValue(eqList)
         }
     }
 
-    private fun fetchEarthquakes(){
+    private fun parseStringToJson(response: String): MutableList<Earthquake> {
+        val responseJson = JSONObject(response)
+        val features = responseJson.getJSONArray("features")
+
+        val eqList = mutableListOf<Earthquake>()
+        for (i in 0 until features.length()) {
+            val feature = features[i] as JSONObject
+            Log.i("feature",feature.toString())
+            val id = feature.getString("id")
+            val properties = feature.getJSONObject("properties")
+            val magnitude = properties.getDouble("mag")
+            val place = properties.getString("place")
+            val time = properties.getLong("time")
+            val geometry = feature.getJSONObject("geometry")
+            val coordinates = geometry.getJSONArray("coordinates")
+            val longitude = coordinates.getDouble(0)
+            val latitude = coordinates.getDouble(1)
+            val earthquake = Earthquake(id, place, magnitude, time, longitude, latitude)
+            Log.i("earthquake",earthquake.toString())
+            eqList.add(earthquake)
+        }
+        return eqList
+
+    }
+
+    private fun fetchEarthquakes() {
 
     }
     //Los metodos que se llamen dentro de una corutina debe llevar suspend
